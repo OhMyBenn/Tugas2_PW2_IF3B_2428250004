f9b3a422879b0a8ecec19addd9a846d0983b39c4
RubenAlfandoWhite <rubenalfandowhite_2428250004@mhs.mdp.ac.id>
2025-11-01 21:11:19 +0700

    npm run build

---
+++ build/assets/Home-0sGdu50o.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-C3PDlDE6.js";function r(){return t.jsxs("div",{children:[t.jsx("h2",{children:"Home"}),t.jsx("p",{children:"Selamat Datang di VitMart"})]})}export{r as default};
